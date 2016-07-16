29b58fa4c1581b564086f8f1541975bf93068469
David-Retina15 <david77115@gmail.com>
2016-07-16 10:40:48 +0800

    first commit

---
+++ src/CreateImage.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by david on 2016/7/15.
+ */
+public class CreateImage extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
